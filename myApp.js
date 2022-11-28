37e1c34c1888e3e3db88cdb20cf2dbf08237e177
Ruben Rossvold <114492580+Rossvold@users.noreply.github.com>
2022-11-28 13:28:08 +0000

    Task #4 Serve static content

--- myApp.js
+++ myApp.js
@@ -4,15 +4,12 @@ let app = express();
 /**Task 1 Meet the node Console. */
 console.log("Hello World");
 
-/**Task 2 Start a Working Express Server */
+/**Task 2-3 Start a Working Express Server & Serve HTML element */
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 })
-
-
-
-
-
+/**Task 4 Serve Static Assets */
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
